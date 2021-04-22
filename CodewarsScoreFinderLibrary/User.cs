f1d3f5c78c3cc99b5cd1ba0fd53c38244faee825
Cruz Sanchez <cruzsanchez9209@gmail.com>
2021-04-21 21:05:03 -0500

    Made the name property on the User class a full property to apply checking for null and empty, setting Name to "Not Provided" if either

--- CodewarsScoreFinderLibrary/User.cs
+++ CodewarsScoreFinderLibrary/User.cs
@@ -7,7 +7,23 @@ namespace CodewarsScoreFinderLibrary
     public class User
     {
         public string UserName { get; set; }
-        public string Name { get; set; }
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set 
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+                else
+                {
+                    name = "Not Provided";
+                }
+            }
+        }
+
         public int Honor { get; set; }
         public string Clan { get; set; }
         public int? LeaderBoardPosition { get; set; }
