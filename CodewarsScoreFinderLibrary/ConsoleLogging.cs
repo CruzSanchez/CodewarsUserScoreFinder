71111e9ce123f5fd0275961e7d8d5c9e21195e5f
Cruz Sanchez <cruzsanchez9209@gmail.com>
2021-04-21 23:15:48 -0500

    Added a never ending Ascii Art Generator. Runs asynchronously and never ends. This also resets the cursor position so the console will not scroll anymore lol.

--- CodewarsScoreFinderLibrary/ConsoleLogging.cs
+++ CodewarsScoreFinderLibrary/ConsoleLogging.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CodewarsScoreFinderLibrary
 {
     public static class ConsoleLogging
     {
+        private static Random rng = new Random();
+
         public static void PrintUserData()
         {
+            int consoleLeft = 50;
+            int consoleTop = 10;
             if (ScoreRetriever.Users.Count > 0)
             {
                 foreach (User user in ScoreRetriever.Users.OrderByDescending(x => x.Honor).ThenBy(x => x.Name))
                 {
+                    Console.SetCursorPosition(consoleLeft, consoleTop);
                     ConfigureUserDataOnConsole(user);
                     NewLine();
+                    consoleTop += 2;
                 }
             }
             else
@@ -31,8 +39,8 @@ namespace CodewarsScoreFinderLibrary
 
         private static void DisplayUserInformation(User user)
         {
-            Console.WriteLine($"Name: {user.Name ??= "Unknown"} || Username: {user.UserName} || Honor: {user.Honor} || " +
-                                    $"TotalCompleted: {user.CodeChallenges.TotalCompleted}");            
+            Console.WriteLine($"Name: {user.Name ??= "Unknown",-20} ||\t Username: {user.UserName,-25} ||    Honor: {user.Honor,6}    ||    " +
+                                    $"TotalCompleted: {user.CodeChallenges.TotalCompleted,5}");
         }
 
         private static void ConfigureUserDataOnConsole(User user)
@@ -54,8 +62,8 @@ namespace CodewarsScoreFinderLibrary
                 case "purple":
                     SetConsoleColor(ConsoleColor.Magenta);
                     DisplayUserInformation(user);
-                    break;                
-            }            
+                    break;
+            }
         }
 
         private static void NewLine()
@@ -66,6 +74,31 @@ namespace CodewarsScoreFinderLibrary
         private static void SetConsoleColor(ConsoleColor consoleColor)
         {
             Console.ForegroundColor = consoleColor;
+        }
+
+        public static void PrintAsciiArt()
+        {
+            var task = Task.Factory.StartNew(async () =>
+            {
+                while (true)
+                {
+                    Console.SetCursorPosition(0,0);
+                    Console.WriteLine(@"      ::::::::   ::::::::  :::::::::  ::::::::::  :::       :::     :::     :::::::::   ::::::::          ::::::::   ::::::::   ::::::::  :::::::::  ::::::::::          :::::::::: ::::::::::: ::::    ::: :::::::::  :::::::::: :::::::::", Console.ForegroundColor = ColorSwap());
+                    Console.WriteLine(@"    :+:    :+: :+:    :+: :+:    :+: :+:         :+:       :+:   :+: :+:   :+:    :+: :+:    :+:        :+:    :+: :+:    :+: :+:    :+: :+:    :+: :+:                 :+:            :+:     :+:+:   :+: :+:    :+: :+:        :+:    :+:");
+                    Console.WriteLine(@"   +:+        +:+    +:+ +:+    +:+ +:+         +:+       +:+  +:+   +:+  +:+    +:+ +:+               +:+        +:+        +:+    +:+ +:+    +:+ +:+                 +:+            +:+     :+:+:+  +:+ +:+    +:+ +:+        +:+    +:+ ");
+                    Console.WriteLine(@"  +#+        +#+    +:+ +#+    +:+ +#++:++#    +#+  +:+  +#+ +#++:++#++: +#++:++#:  +#++:++#++        +#++:++#++ +#+        +#+    +:+ +#++:++#:  +#++:++#            :#::+::#       +#+     +#+ +:+ +#+ +#+    +:+ +#++:++#   +#++:++#:   ");
+                    Console.WriteLine(@" +#+        +#+    +#+ +#+    +#+ +#+         +#+ +#+#+ +#+ +#+     +#+ +#+    +#+        +#+               +#+ +#+        +#+    +#+ +#+    +#+ +#+                 +#+            +#+     +#+  +#+#+# +#+    +#+ +#+        +#+    +#+   ");
+                    Console.WriteLine(@"#+#    #+# #+#    #+# #+#    #+# #+#          #+#+# #+#+#  #+#     #+# #+#    #+# #+#    #+#        #+#    #+# #+#    #+# #+#    #+# #+#    #+# #+#                 #+#            #+#     #+#   #+#+# #+#    #+# #+#        #+#    #+#    ");
+                    Console.WriteLine(@"########   ########  #########  ##########    ###   ###   ###     ### ###    ###  ########          ########   ########   ########  ###    ### ##########          ###        ########### ###    #### #########  ########## ###    ###     ");
+                    await Task.Delay(250);
+                }
+            });
+        }
+
+        private static ConsoleColor ColorSwap()
+        {
+            var consoleColors = Enum.GetValues(typeof(ConsoleColor));
+            return (ConsoleColor)consoleColors.GetValue(rng.Next(1, consoleColors.Length - 1));
         }
     }
 }
--- CodewarsUserScoreFinderConsoleUI/Program.cs
+++ CodewarsUserScoreFinderConsoleUI/Program.cs
@@ -1,13 +1,30 @@
 ﻿using CodewarsScoreFinderLibrary;
 using System;
+using System.Runtime.InteropServices;
 
 namespace CodewarsUserScoreFinderConsoleUI
 {
     class Program
     {
+        #region Console Window Settings
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        private static IntPtr ThisConsole = GetConsoleWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int HIDE = 0;
+        private const int MAXIMIZE = 3;
+        private const int MINIMIZE = 6;
+        private const int RESTORE = 9;
+        #endregion
         static void Main(string[] args)
         {
-            Console.Clear();            
+            ShowWindow(ThisConsole, MAXIMIZE);
+
+            Console.Clear();
+
+            ConsoleLogging.PrintAsciiArt();
 
             ScoreRetriever retriever = new ScoreRetriever();
 
